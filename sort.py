93bc88be8fa8f22045f5efa2c87913e5c033f2e7
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-02-26 14:38:52 +0200

    Implements insertion sort algorithm

--- sort.py
+++ sort.py
@@ -39,3 +39,22 @@ with open("./saida.txt", "r") as file:
         lista[i] = temp
 
 print(lista)
+
+#Insertion sort
+
+with open("./saida.txt", "r") as file:
+    lista = []
+    for line in file:
+        lista.append(line)
+
+for i in range(1, len(lista)):
+    chave = lista[i]
+    j = i - 1
+    
+    while(j >= 0) and (lista[j] > chave):
+        lista[j + 1] = lista[j]
+        j -= 1
+
+    lista[j + 1] = chave
+
+print(lista)
