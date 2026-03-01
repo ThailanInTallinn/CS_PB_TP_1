92e364cb561ca9bd74014b69354395ac54e7a210
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-01 15:19:32 +0200

    feat: count the steps taken for each action on the hashtable

--- hashtable.py
+++ hashtable.py
@@ -9,33 +9,49 @@ class Hashtable:
         return chave % self.tamanho
 
     def inserir(self, chave, valor):
+        contador = 0
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
 
         for i, (chave_ex, _) in enumerate(bucket):
+            contador += 1
             if(chave_ex == chave):
                 bucket[i] = (chave, valor)
+                print(f"Numero de passos para inserção com chave existente: {contador}")
                 return 
             
         bucket.append((chave, valor))
+        contador += 1
+        print(f"Numero de passos para inserção sem chave existente: {contador}")
+
+
 
     def buscar(self, chave):
+        contador = 0
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
 
         for chave_ex, valor in bucket:
+            contador += 1
             if(chave_ex == chave):
+                print(f"Numero de passos para busca bem-sucedida: {contador}")
                 return valor
 
+        print(f"Numero de passos para busca que não retornou resultado: {contador}")
         return None
 
     def remover(self, chave):
+        contador = 0
         index = self._funcao_hash(chave)
         bucket = self.tabela[index]
 
         for i, (chave_ex, valor) in enumerate(bucket):
+            contador += 1
             if chave_ex == chave:
+                print(f"Numero de passos para remoção bem-sucedida: {contador}")
                 del bucket[i]
+
+        print(f"Numero de passos para remoção malssucedida: {contador}")
 
         return None
 
