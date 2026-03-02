58003d6e181206f99d14de216f45bbaf364bb98d
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-02 15:42:07 +0200

    feat: count the steps taken for each action on both queue and stack

--- fila.py
+++ fila.py
@@ -3,26 +3,36 @@ tamanho_max = 100
 contador = 0
 
 def adicionar(fila, item, count):
+    counter = 0
     if(count >=tamanho_max):
+        counter += 1
         print("Fila já atingiu seu tamanho máximo.")
+        print(f"Numero de passos para adição malssucedida: {counter}")
         return -1
     
     fila[count] = item
+    counter += 1
+    print(f"Numero de passos para adicao feita com sucesso: {counter}")
     return 1
 
 def remover(fila):
+    counter = 0
     if(fila[0] == "\0"):
+        counter += 1
         print("Fila vazia.")
+        print(f"Numero de passos para remoçao malssucedida: {counter}")
         return "\0"
 
     primeiro_elemento = fila[0]
-
-    counter = len(fila)
+    counter += 1
 
     for i in range(1, tamanho_max - 1):
         fila[i - 1] = fila[i]
+        counter += 1
         fila[i] = "\0"
-
+        counter += 1
+        
+    print(f"Numero de passos para remoção feita com sucesso: {counter}")
     return primeiro_elemento
 
 
--- pilha.py
+++ pilha.py
@@ -5,20 +5,31 @@ def top(pilha):
 
 
 def adicionar(item, pilha):
+    counter  = 0
     tip = top(pilha)
+    counter += 1
     if(tip == tamanho_max):
+        counter += 1
         print("Tamanho máximo atingido.")
+        print(f"Numero de passos para adição malssucedida: {counter}")
         return -1
 
     pilha.append(item) 
+    counter += 1
+    print(f"Numero de passos para adição feita com sucesso: {counter}")
     return 1
 
 def remover(pilha):
+    counter = 0
     if(top(pilha) == 0):
+        counter += 1
         print("Pilha vazia.")
+        print(f"Numero de passos para remoção malssucedida: {counter}")
         return -1
 
     removed = pilha.pop()
+    counter += 1
+    print(f"Numero de passos para remoção feita com sucesso: {counter}")
     return removed
 
 pilha = []
